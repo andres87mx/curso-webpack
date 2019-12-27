002edef2ab481a31786a567adc50bf297b7cc16b
Andres Ruiz <aruiz@grupooctano.com.mx>
2019-12-27 13:08:40 -0600

    Trabajando con React

--- webpack-4/react/src/components/app.js
+++ webpack-4/react/src/components/app.js
@@ -1,9 +1,21 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import data from './data.json'
+import Loader from './loader'
 function App() {
+    const [LoaderList, setLoaderList] = useState([])
+    function handleClick() {
+        setLoaderList(data.loaders)
+    }
     return (
         <div>
             que linda aplicacion hecha en react.js
+            <ul>
+                {
+                    LoaderList.map(item => <Loader {...item} key={item.id} />)
+                }
+            </ul>
+            <button onClick={handleClick} >Mostrar lo aprendido hasta el momento</button>
+            
         </div>
     )
 }
