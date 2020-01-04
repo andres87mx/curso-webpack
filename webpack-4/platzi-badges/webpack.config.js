da88def565c52d483d853f1676d857222d88a41e
Andres Ruiz <aruiz@grupooctano.com.mx>
2020-01-04 08:56:19 -0600

    Versionando, limpiando y comprimiendo archivos

--- webpack-4/platzi-badges/webpack.config.js
+++ webpack-4/platzi-badges/webpack.config.js
@@ -3,6 +3,9 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const webpack = require('webpack')
 const AddAssetHtmlPlugin = require('add-asset-html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const TerserJSPlugin = require('terser-webpack-plugin')
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin')
 
 module.exports = {
     entry: {
@@ -10,11 +13,16 @@ module.exports = {
     },
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'js/[name].js',
+        filename: 'js/[name].[hash].js',
         publicPath: 'http://localhost:3001/',
         chunkFilename: 'js/[id].[chunkhash].js'
     },
-
+    optimization: {
+        minimizer: [
+            new TerserJSPlugin(),
+            new OptimizeCssAssetsPlugin()
+        ]
+    },
     module: {
         rules: [
             {
@@ -36,7 +44,9 @@ module.exports = {
                 use: {
                     loader: 'url-loader',
                     options: {
-                        limit: 1000
+                        limit: 1000,
+                        name:'[hash].[ext]',
+                        outputPath: 'assets'
                     }
                 }
             }
@@ -44,8 +54,8 @@ module.exports = {
     },
     plugins: [
         new MiniCssExtractPlugin({
-            filename: 'css/[name].css',
-            chunkFilename: 'css/[id].css'
+            filename: 'css/[name].[hash].css',
+            chunkFilename: 'css/[id].[hash].css'
         }),
         new HtmlWebpackPlugin({
             template: path.resolve(__dirname, 'public/index.html')
@@ -57,6 +67,9 @@ module.exports = {
             filepath: path.resolve(__dirname, 'dist/js/*.dll.js'),
             outputPath: 'js',
             publicPath: 'http://localhost:3001/js'
+        }),
+        new CleanWebpackPlugin({
+            cleanOnceBeforeBuildPatterns: ['**/app.*']
         })
     ]
 }
